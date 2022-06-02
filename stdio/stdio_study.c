543690fa043a36120f06005a96809309d331dee2
jack.aliyun <jxhczal@163.com>
2022-06-02 08:46:42 +0800

    mod stdio_study.c

--- stdio/stdio_study.c
+++ stdio/stdio_study.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+int get_sum(int num_of_param, ...)
+{
+    int loop = 0;
+    int sum = 0;
+    va_list p_va_list;
+    va_start(p_va_list, num_of_param);
+    for (loop = 0; loop < num_of_param; loop++)
+    {
+        sum += va_arg(p_va_list, int);
+    }
+
+    va_end(p_va_list);
+    return sum;
+}
 
 int main()
 {
@@ -13,5 +29,35 @@ int main()
     snprintf(buf, 5, "%d-%d-%d", year, month, date);
     puts(buf);
 
+    FILE *p_file;
+    p_file = fopen("temp.txt","r");
+    if (NULL == p_file)
+    {
+        printf("fopen failed\n");
+        return -1;
+    }
+    FILE *p_file_to_write;
+    p_file_to_write = fopen("temp_copy.txt", "w");
+    if (NULL == p_file_to_write)
+    {
+        printf("fopen failed\n");
+        return -1;
+    }
+
+    char s[50];
+
+    char* ret_s = fgets(s, 50, p_file);
+    while(NULL != ret_s)
+    {
+        fputs(ret_s, p_file_to_write);
+        ret_s = fgets(s, 50, p_file);
+    }
+    
+    fclose(p_file);
+    fclose(p_file_to_write);
+
+    //可变参数函数
+    int sum = get_sum(3, 1, 2, 3);
+    printf("sum=%d\n",sum);
     return 0;
 }
