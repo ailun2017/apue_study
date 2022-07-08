a74c4ad8a41dddeb2da585e98bfdce19b0482b9a
jack.aliyun <jxhczal@163.com>
2022-07-08 10:56:19 +0800

    mod system_io_study.c

--- stdio/system_io_study.c
+++ stdio/system_io_study.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 
 
 void compare_sysio_and_stdio()
@@ -14,7 +15,9 @@ void compare_sysio_and_stdio()
     write(1, "b", 1);
 }
 
-#define BUFFSIZE 1024
+#define BUFFSIZE 1024*1000*2
+char buf[BUFFSIZE];
+
 void my_copy(int argc, char* argv[])
 {
 	if (argc < 3)
@@ -27,7 +30,6 @@ void my_copy(int argc, char* argv[])
 	int len;
 	int ret;
 	int pos;
-	char buf[BUFFSIZE];
 
 	sfd = open(argv[1], O_RDONLY);
 	if (sfd < 0)
@@ -54,7 +56,7 @@ void my_copy(int argc, char* argv[])
 	while(1)
 	{
 		len = read(sfd, (void*)&buf[0], BUFFSIZE);
-		printf("len = %d\n", len);
+		//printf("len = %d\n", len);
 		if (len < 0)
 		{
 			perror("read()");
@@ -69,7 +71,7 @@ void my_copy(int argc, char* argv[])
 		while(len > 0)
 		{
 			ret = write(dfd, buf + pos, len);
-			printf("ret = %d\n", ret);
+			//printf("ret = %d\n", ret);
 			if (ret < 0)
 			{
 				perror("write()");
@@ -85,9 +87,115 @@ void my_copy(int argc, char* argv[])
 	close(dfd);
 }
 
+void delete_line(char* filename , int linenum)
+{
+	int rd_fd, wr_fd;
+	char buff[BUFFSIZE];
+	memset(buff, '\0', sizeof(buff));
+	int count = 0;
+	int len = 0;
+	int pos = 0;
+	int ret = 0;
+	int len_before_linenum = 0;
+	rd_fd = open(filename, O_RDONLY);
+	if (rd_fd < 0)
+	{
+		perror("open()");
+		return;
+	}
+	len_before_linenum = 1;
+	count = 0;
+	while(1)
+	{
+		if (count < linenum)
+		{
+			len = read(rd_fd, buff, 1);
+			if (len < 0)
+			{
+				perror("read()");
+				break;
+			}
+			if (0 == len)
+			{
+				printf("len == 0\n");
+				break;
+			}
+		
+			if ('\n' == buff[0])
+			{
+				count++;
+			}
+			if (count < (linenum-1))
+			{
+				len_before_linenum++;
+			}
+		}
+		else
+		{
+			len = read(rd_fd, buff+pos, 1);			
+            if (len < 0)
+            {
+                perror("read()");
+                break;
+            }
+            if (0 == len)
+            {
+                printf("len == 0\n");
+                break;
+            }
+			pos++;
+		}
+	}
+	//printf("buf: %s", buff);
+	wr_fd = open(filename, O_WRONLY);
+	if (wr_fd < 0)
+	{
+		perror("open()");
+		close(rd_fd);
+		return;
+	}
+	ftruncate(wr_fd, len_before_linenum+1);
+	//printf("after trunc: %s", buff);
+	printf("len_before_linenum %d, pos %d\n", len_before_linenum, pos);
+	lseek(wr_fd, len_before_linenum, SEEK_SET);
+	len = pos + 1;
+	pos = 0;
+	while(len > 0)
+	{
+		ret = write(wr_fd, buff+pos, len);
+
+		if (ret < 0)
+		{
+			perror("write()");
+			break;
+		}
+		printf("ret %d\n", ret);
+		len -= ret;
+		pos += ret;
+	}
+
+	close(rd_fd);
+	close(wr_fd);
+}
+
 int main(int argc, char* argv[])
 {
-	compare_sysio_and_stdio();
-	my_copy(argc, argv);
+	if (0)
+	{
+		compare_sysio_and_stdio();
+	}
+	if (0)
+	{
+		my_copy(argc, argv);
+	}
+	if (1)
+	{
+		if (argc < 2)
+		{
+			printf("usage...\n");
+			exit(-1);
+		}
+		delete_line(argv[1], 3);
+	}
 	exit(0);
 }
